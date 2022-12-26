a1c8b1bb0d142282116707482acfff3fe5d3b8c4
Francisco Cabezas <frank_vega25@hotmail.com>
2022-12-26 14:15:44 -0500

    Create Test7.js

---
+++ cypress/integration/examples/Test7.js
@@ -0,0 +1,14 @@
+/// <reference types="Cypress" />
+
+describe('My Seven Test Suite', () => {
+    it('My SevenTest case', () => {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice')
+
+        // prop url visit
+        cy.get('#opentab').then(function ($el) {
+            const url = $el.prop('href')
+            cy.log(url)
+            cy.visit(url)
+        })
+    })
+})
