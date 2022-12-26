eaacdf4ae5d7ad5f51bff1e33e5d380f1e042090
Francisco Cabezas <frank_vega25@hotmail.com>
2022-12-26 13:45:18 -0500

    Create Test6.js

---
+++ cypress/integration/examples/Test6.js
@@ -0,0 +1,12 @@
+/// <reference types="Cypress" />
+
+describe('My Six Test Suite', () => {
+    it('My SixTest case', () => {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice')
+
+        // Mouse Hover Event
+        cy.get('.mouse-hover-content').invoke('show')
+        cy.contains('Top').click()
+        cy.url().should('include', 'top')
+    })
+})
