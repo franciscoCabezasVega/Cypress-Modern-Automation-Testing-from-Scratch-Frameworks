c2a86f092558cdb95784fd7c55768bfaa576f59d
Francisco Cabezas <frank_vega25@hotmail.com>
2022-12-26 14:49:37 -0500

    create Test8.js

---
+++ cypress/integration/examples/Test8.js
@@ -0,0 +1,16 @@
+/// <reference types="Cypress" />
+/// <reference types="cypress-iframe" />
+
+import 'cypress-iframe';
+
+describe('My Seven Test Suite', () => {
+    it('My SevenTest case', () => {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice')
+
+        // iframe in cypress
+        cy.frameLoaded('#courses-iframe')
+        cy.iframe().find("a[href*='mentorship']").eq(0).click() // eq(0) permite definir a que elemento le vamos a dar click
+        cy.wait(2000)
+        cy.iframe().find("h1[class*='pricing-title']").should('have.length', 2)
+    })
+})
